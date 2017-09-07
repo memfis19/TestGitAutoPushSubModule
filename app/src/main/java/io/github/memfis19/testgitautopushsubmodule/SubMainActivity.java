9a48e040a8880026bfe062f7ef0aefa3d9f2663a
Rodion Surzhenko <rodion.surzhenko@gmail.com>
2017-09-07 22:40:03 +0300

    initial commit

---
+++ app/src/main/java/io/github/memfis19/testgitautopushsubmodule/SubMainActivity.java
@@ -0,0 +1,13 @@
+package io.github.memfis19.testgitautopushsubmodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_main);
+    }
+}
