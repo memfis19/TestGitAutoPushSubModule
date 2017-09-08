67055118b1d34b1eb950f1b902775c8e869b018a
Rodion Surzhenko <rodion.surzhenko@activewindow.dk>
2017-09-08 11:35:24 +0300

    changes

--- app/src/main/java/io/github/memfis19/testgitautopushsubmodule/SubMainActivity.java
+++ app/src/main/java/io/github/memfis19/testgitautopushsubmodule/SubMainActivity.java
@@ -9,5 +9,6 @@ public class SubMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_main);
+        //123
     }
 }
